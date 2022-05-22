3e3c88cec352d22181b54b1b0b56d0e3c2118769
Sabeel Khan <sabeelhps@gmail.com>
2022-05-22 11:33:01 +0530

    Lecture-12

---
+++ Lecture-11/Conditonals/app.js
@@ -0,0 +1,45 @@
+
+
+let name = 'Kartik';
+
+let myname = name || 'Anonymous';
+
+
+
+if (10 > 9) {
+    console.log("Hello");
+}
+
+
+// prompt("Enter a number");
+
+// let a = 10;
+// let b = 20;
+
+// if (a < b) {
+//     console.log("b is greater");
+// }
+// else if (a==b) {
+//     console.log('a is equal to b')
+// }
+// else if (a!=b) {
+//     console.log('a is not equal to b')
+// }
+// else{
+//     console.log('a is greater')
+// }
+
+
+
+const num = prompt("Enter a number");
+console.log(typeof num);
+alert(`You entered ${num}`);
+
+
+
+
+
+
+
+
+
---
+++ Lecture-11/StringMethods/app.js
@@ -0,0 +1,6 @@
+
+
+// console.log("Hello World!");
+// console.warn("This is a Warning!");
+// console.error("This is a error");
+// console.dir("Hello World");
