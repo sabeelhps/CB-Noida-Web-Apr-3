4c29d6491ccc93bf96be3181c728f56521838bc1
Sabeel Khan <sabeelhps@gmail.com>
2022-06-12 12:38:36 +0530

    Lecture-18

---
+++ Lecture-18/AsyncProgramming/app.js
@@ -0,0 +1,82 @@
+
+
+// console.log("START");
+
+// function fun() {
+//     console.log('Inside fun');
+// }
+
+// function delay(n) {
+
+//     for (let i = 1; i <= n; i++){
+//         const x = new Date().getTime();
+//         while (new Date().getTime() < x + 1000) { }
+//     }
+   
+//     fun();
+// }
+
+// delay(5);
+
+// console.log("END");
+
+
+// ---------------------------------------------------------
+
+
+// console.log("START");
+
+// setTimeout(function cb() { 
+//     console.log('Inside cb!');
+// }, 8000);
+
+
+// const id = setInterval(() => {
+//     console.log('Inside setTimout');
+// }, 1000);
+
+// clearInterval() and clearTimeout() are used to clear the timers
+
+// console.log("END");
+
+// ----------------------------------------Imp Question----
+
+
+// 1.
+
+// console.log("START");
+
+// let magicNumber = 37;
+
+
+// setTimeout(() => {
+//     console.log("Inside cb")
+// }, 3000);
+
+
+// while (true) {
+//     console.log("Inside while");
+// }
+
+// console.log("END");
+
+
+
+
+// 2.
+
+
+// for (var i = 0; i < 3; i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 1);
+// }
+
+// 3.
+
+// for ( i = 0; i < 3; i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 1);
+// }
+
