96d174971739422bbd25a562270ebfc40b739b77
Sabeel Khan <sabeelhps@gmail.com>
2022-07-02 12:59:32 +0530

    Lecture-23

--- Lecture-23/Debouncing&Throttling/app.js
+++ Lecture-23/Debouncing&Throttling/app.js
@@ -1,23 +1,14 @@
 const input = document.querySelector('input');
-
-
-
-
-
-
-
-
 let count = 0;
 
-function getData() {
+function apiCall() {
     console.log('Fetching Data', count++);
 }
 // Debouncing
-
 function debouncing(func, delay) {
     let timer;
     return function () {
-        clearInterval(timer);
+        clearTimeout(timer);
         timer = setTimeout(() => {
             func();
         }, delay);
@@ -25,13 +16,26 @@ function debouncing(func, delay) {
 }
 
 // input.addEventListener('keypress',getData);
-const betterFunction = debouncing(getData, 300);
+const betterFunction = debouncing(apiCall, 300);
 // console.log(betterFunction);
-input.addEventListener('keypress', betterFunction);
+// input.addEventListener('keypress', betterFunction);
 
 
-function add(x, y) {
-    return x + y;
+// ------------------Throttling-------------------
+
+
+function throttling(func, delay) {
+    let prev = 0;
+    return function () {
+        const now = new Date().getTime();
+        if (now - prev > delay) {
+            prev = now;
+            func();
+        }
+    }
 }
 
-const res = add(1, 2);
+
+const betterThrottlingFunction = throttling(apiCall, 400);
+input.addEventListener('keypress', betterThrottlingFunction);
+
