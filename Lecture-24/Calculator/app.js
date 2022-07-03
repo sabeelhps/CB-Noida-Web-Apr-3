dd4697345f6700e19f01f97320c908899b5f394d
Sabeel Khan <sabeelhps@gmail.com>
2022-07-03 11:59:26 +0530

    Lecture-24

---
+++ Lecture-24/Calculator/app.js
@@ -0,0 +1,28 @@
+const keypad = document.getElementById('keypad');
+const screen = document.getElementById('screen');
+
+keypad.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'BUTTON') {
+        // console.log(event.target);
+        const buttonText = event.target.innerText;
+
+        if (buttonText === 'C') {
+            screen.value = "";
+        }
+        else if (buttonText === 'x') {
+            screen.value += "*";
+        }
+        else if (buttonText === '=') {
+            try {
+                screen.value=eval(screen.value);
+            }
+            catch (e) {
+                screen.value = "Invalid Operation";
+            }
+            
+        }
+        else {
+            screen.value += buttonText;
+        }  
+    }
+});
